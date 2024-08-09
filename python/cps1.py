22dbe7cba6e0797f8c0be8fa8b887b6fb26638f0
duxiuxing <duxiuxing@foxmail.com>
2024-08-09 08:56:01 +0800

    cps1: update wii app and channel

--- python/cps1.py
+++ python/cps1.py
@@ -6,12 +6,16 @@ wii_app_files_tuple = (
     "apps\\ra-cps1\\boot.dol",
     "apps\\ra-cps1\\icon.png",
     "apps\\ra-cps1\\meta.xml",
+    "apps\\sf2",
+    "apps\\sf2ce",
     "private",
     "wad\\ra-cps1\\Capcom-CPS-1-RunningSnakes-Gold [C1LR].wad",
     "wad\\ra-cps1\\Capcom-CPS-1-RunningSnakes-White [C1LR].wad",
     "wad\\ra-cps1\\Capcom-CPS-1-RunningSnakes-Yellow [C1LR].wad",
     "wad\\ra-cps1\\R-Sam-CPS1-White [C1LR].zhtw.wad",
-    "wad\\ra-cps1\\R-Sam-CPS1-Yellow [C1LR].zhtw.wad"
+    "wad\\ra-cps1\\R-Sam-CPS1-Yellow [C1LR].zhtw.wad",
+    "wad\\Street Fighter II [SF21].zhtw.wad",
+    "wad\\Street Fighter II CE [SF22].zhtw.wad"
 )
 
 cps1 = CPS(1)
