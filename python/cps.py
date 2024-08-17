45f2f8e1cc3bf2770512e5cee63aab12b265ac96
duxiuxing <duxiuxing@foxmail.com>
2024-08-18 00:15:04 +0800

    cps all: update python

--- python/cps.py
+++ python/cps.py
@@ -2,16 +2,18 @@
 
 import os
 
-from console_base import ConsoleBase
+from console_impl import ConsoleImpl
 from local_configs import LocalConfigs
 from wiiflow import WiiFlow
 
 
-class CPS(ConsoleBase):
+class CPS(ConsoleImpl):
     def __init__(self, version_number):
         super().__init__()
         self.version_number = version_number
-        self.wiiflow = WiiFlow(self, f"CPS{self.version_number}")
+
+    def create_wiiflow(self):
+        return WiiFlow(self, f"CPS{self.version_number}")
 
     def root_folder_path(self):
         return os.path.join(LocalConfigs.repository_folder_path(), f"cps{self.version_number}")
